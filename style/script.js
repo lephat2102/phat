a5cf90538e7af57add047649eeb5e72a63b7d1b1
lephat2102 <lep87764@gmail.com>
2025-10-23 13:28:48 +0700

    Update script.js

--- style/script.js
+++ style/script.js
@@ -1279,7 +1279,7 @@ window.addEventListener('orientationchange', () => {
   // Fallback: sau 8s từ load, hiển thị nếu chưa có sự kiện
   window.addEventListener('load', ()=>{
     setTimeout(()=>{
-     if (overlay.classList.contains('hidden')) showInvitation();
+      if (overlay.classList.contains('hidden')) showInvitation();
     }, 8000);
   });
 })();
@@ -1363,6 +1363,7 @@ window.addEventListener('orientationchange', () => {
   respOverlay.addEventListener('click', (e) => { if (e.target === respOverlay) hideOverlay(respOverlay, respModal); });
 
 })();
+
 
 
 
